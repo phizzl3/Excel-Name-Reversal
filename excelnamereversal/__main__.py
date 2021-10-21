f5f759e8b2e910c3f4e93dd40f4022f3d5429b7c
Brandon <phizzl3@gmail.com>
2021-10-21 16:00:32 -0500

    Initial Commit

---
+++ excelnamereversal/__main__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""
+* Get file
+* Generate Xlsx input and output
+* Call reversal and get output file
+* Save output file
+* Open outputfile (optional)
+"""
---
+++ excelnamereversal/read_reverse_write.py
@@ -0,0 +1,7 @@
+"""
+* Get input and output file objects
+* Read data
+* Reverse data
+* Write data to output
+* Return output
+"""
